ab4f9bd143213b5f040540b389a9da85679639d0
Gilvan Ferreira da Luz <125775442+gilvanfdaluz@users.noreply.github.com>
2023-03-04 21:05:01 -0300

    atualizacao

---
+++ Criando Objetos e Classs em Java Script/aula1.js
@@ -0,0 +1,15 @@
+const vitor = {
+  nome: 'Vitor',
+  idade: 25,
+  altura: 1.69,
+};
+
+/*
+console.log(vitor.nome);
+console.log(vitor.idade);
+console.log(vitor)
+*/
+
+vitor.altura = 1.69
+delete vitor.nome
+console.log(vitor);
